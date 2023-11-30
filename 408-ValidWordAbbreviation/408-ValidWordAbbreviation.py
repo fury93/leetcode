c2c0dbb1a5db6b74bdb129484b93eea6c57de268
fury93 <www.fanreala@gmail.com>
2023-11-30 16:02:02 +0300

    Update file 30/11/2023, 16:02:02

--- 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
+++ 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
@@ -1,28 +1,19 @@
 "
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        def getShift(j):
-            start = j
-            while j < len(abbr) and abbr[j].isdigit():
-                j += 1
-            return int(abbr[start:j]), j
+        i, j, l1, l2 = 0, 0, len(word), len(abbr)
+        while i < l1 and j < l2:
+            elif abbr[j] != '0':
+                start = j
+                while j < l2 and abbr[j].isdigit():
+                i += int(abbr[start:j])
 
-        i, j = 0, 0
-        while i < len(word) and j < len(abbr):
-            if abbr[j].isdigit():
-                if abbr[j] == '0': return False
-                shift, j = getShift(j)
-                i += shift
-            
-            if i >= len(word) or j >= len(abbr): break
-            
-            if word[i] != abbr[j]:
-                return False
-            
-            i, j = i + 1, j + 1
-
-        return i == len(word) and j == len(abbr)
-        
-
+        return i == l1 and j == l2
+            if abbr[j].isalpha():
+                if word[i] != abbr[j]: break
+                i, j = i + 1, j + 1
+            else:
+                break
+                    j += 1
 
 0
