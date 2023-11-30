4d24eabdaf78eb807745df91d6aec3eee41b50a6
fury93 <www.fanreala@gmail.com>
2023-11-30 16:18:57 +0300

    Update 408-ValidWordAbbreviation.py

--- 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
+++ 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
@@ -1,20 +1,18 @@
-"
-class Solution:
-    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        i, j, l1, l2 = 0, 0, len(word), len(abbr)
-        while i < l1 and j < l2:
-            elif abbr[j] != '0':
-                start = j
-                while j < l2 and abbr[j].isdigit():
-                i += int(abbr[start:j])
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i, j, l1, l2 = 0, 0, len(word), len(abbr)
 
-        return i == l1 and j == l2
-            if abbr[j].isalpha():
-                if word[i] != abbr[j]: break
-                i, j = i + 1, j + 1
-            else:
-                break
-                    j += 1
+        while i < l1 and j < l2:
+            if abbr[j].isalpha():
+                if word[i] != abbr[j]: break
+                i, j = i + 1, j + 1
+            elif abbr[j] != '0':
+                start = j
+                while j < l2 and abbr[j].isdigit():
+                    j += 1
+                i += int(abbr[start:j])
+            else:
+                break
 
-        
-0
+        return i == l1 and j == l2
+        
