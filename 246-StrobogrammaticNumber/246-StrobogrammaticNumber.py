7fb36bf660d1b383ee5e09cc73cda783ea150a50
fury93 <www.fanreala@gmail.com>
2023-11-30 18:54:15 +0300

    Update file 30/11/2023, 18:54:14

--- 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
+++ 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
@@ -4,7 +4,7 @@ class Solution:
         i, j = 0, len(num) - 1
 
 
-    def isStrobogrammatic2(self, num: str) -> bool:
+    
         d = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
         num2 = ''.join(d[n] for n in num if n in d)
         while i <= j:
@@ -14,4 +14,6 @@ class Solution:
             j -= 1
         
         return True
+    def isStrobogrammatic2(self, num: str) -> bool:
+        
         return num == num2[::-1]
