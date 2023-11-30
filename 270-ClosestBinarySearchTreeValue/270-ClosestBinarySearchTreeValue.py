6251851feb314b656d6b21832f3c1fac3970ec8f
fury93 <www.fanreala@gmail.com>
2023-11-30 20:28:04 +0300

    Update file 30/11/2023, 20:28:04

--- 270-ClosestBinarySearchTreeValue/270-ClosestBinarySearchTreeValue.py
+++ 270-ClosestBinarySearchTreeValue/270-ClosestBinarySearchTreeValue.py
@@ -1,26 +1,26 @@
 [
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+   
+    
         res, diff = math.inf, math.inf
         def dfs(node):
             if not node: return
             
+            nonlocal res, diff
             curDiff = abs(node.val - target)
             if curDiff < diff:
                 res = node.val
                 diff = curDiff
-            if target < node.val: dfs(node.left)
-            else: dfs(node.right)
-
-        dfs(root)
-
-
-            nonlocal res, diff
-#     def __init__(self, val=0, left=None, right=None):
-# class TreeNode:
-            elif curDiff == diff:
-                res = min(res, node.val)
+            root = root.left if target < root.val else root.right
         return res
+    def closestValue2(self, root: Optional[TreeNode], target: float) -> int:
+            res = min(res, root.val, key = lambda x: (abs(x - target), x))
+        while root:
+        res = root.val
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+#         self.left = left
+#         self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+# Definition for a binary tree node.
