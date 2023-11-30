ea515f9185bb38649af60d5f5b5f32b868b6f34d
fury93 <www.fanreala@gmail.com>
2023-11-30 18:53:36 +0300

    Update file 30/11/2023, 18:53:35

--- 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
+++ 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
@@ -1,5 +1,17 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
         d = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
+        i, j = 0, len(num) - 1
+
+
+    def isStrobogrammatic2(self, num: str) -> bool:
+        d = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
         num2 = ''.join(d[n] for n in num if n in d)
+        while i <= j:
+            if num[i] not in d or d[num[i]] != num[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
         return num == num2[::-1]
