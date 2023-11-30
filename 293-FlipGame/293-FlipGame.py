43d16a0e4a267e16dbc51f12f1b7242696206ada
fury93 <www.fanreala@gmail.com>
2023-11-30 18:33:48 +0300

    Create file

---
+++ 293-FlipGame/293-FlipGame.py
@@ -0,0 +1,8 @@
+"
+class Solution:
+    def generatePossibleNextMoves(self, states: str) -> List[str]:
+        res = []
+        for i in range(1, len(states)):
+            if states[i] == states[i-1] == '+':
+                res.append(states[:i-1] + '--' + states[i+1:])
+        return res
