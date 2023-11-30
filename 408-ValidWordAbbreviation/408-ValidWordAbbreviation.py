e876fea51ea9d7d37b08cdd4d3c977d609ac5b2d
fury93 <www.fanreala@gmail.com>
2023-11-30 18:34:45 +0300

    Update file 30/11/2023, 18:34:44

--- 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
+++ 408-ValidWordAbbreviation/408-ValidWordAbbreviation.py
@@ -1,10 +1,17 @@
-"
 class Solution:
-    def generatePossibleNextMoves(self, states: str) -> List[str]:
-        res = []
-        for i in range(1, len(states)):
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i, j, l1, l2 = 0, 0, len(word), len(abbr)
 
-            if states[i] == states[i-1] == '+':
-                res.append(states[:i-1] + '--' + states[i+1:])
-        return res
-        
+        while i < l1 and j < l2:
+            if abbr[j].isalpha():
+                if word[i] != abbr[j]: break
+                i, j = i + 1, j + 1
+            elif abbr[j] != '0':
+                start = j
+                while j < l2 and abbr[j].isdigit():
+                    j += 1
+                i += int(abbr[start:j])
+            else:
+                break
+
+        return i == l1 and j == l2
