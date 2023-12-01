b06d0db8f06e47c2d89c4ddd0380cf53a0998991
fury93 <www.fanreala@gmail.com>
2023-12-01 22:18:01 +0300

    Update file 01/12/2023, 22:18:01

--- 1086-HighFive/1086-HighFive.py
+++ 1086-HighFive/1086-HighFive.py
@@ -2,11 +2,11 @@ class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
         students = [None] * 1001
         for idx, score in items:
-            if not students[idx]:
-                students[idx] = []
-            heappush(students[idx], score)
-            if len(students[idx]) > 5:
-                heappop(students[idx])
+            if not students[idx]: students[idx] = []
+            if len(students[idx]) == 5:
+                heappushpop(students[idx], score)
+            else:
+                heappush(students[idx], score)
 
         return [[key, sum(val)//5] for key, val in enumerate(students) if val]
 
