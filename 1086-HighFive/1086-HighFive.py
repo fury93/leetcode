41f9eacbbefed57896eea59f1205d0307fd54ccd
fury93 <www.fanreala@gmail.com>
2023-12-01 22:19:43 +0300

    Update file 01/12/2023, 22:19:43

--- 1086-HighFive/1086-HighFive.py
+++ 1086-HighFive/1086-HighFive.py
@@ -3,11 +3,9 @@ class Solution:
         students = [None] * 1001
         for idx, score in items:
             if not students[idx]: students[idx] = []
-            if len(students[idx]) == 5:
-                heappushpop(students[idx], score)
-            else:
+            if len(students[idx]) < 5:
                 heappush(students[idx], score)
-
+            else:
+                heappushpop(students[idx], score)
+               
         return [[key, sum(val)//5] for key, val in enumerate(students) if val]
-
-
