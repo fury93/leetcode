19d23d65b8a2624202c858ba55e30357fa2d315a
fury93 <www.fanreala@gmail.com>
2023-12-01 23:02:27 +0300

    Update file 01/12/2023, 23:02:27

--- 1708-LargestSubarrayLengthK/1708-LargestSubarrayLengthK.py
+++ 1708-LargestSubarrayLengthK/1708-LargestSubarrayLengthK.py
@@ -1,5 +1,11 @@
+[
 class Solution:
     def largestSubarray(self, nums: List[int], k: int) -> List[int]:
+        mx = max(nums[:len(nums)-k+1])
+        idx = nums.index(mx)
+        return nums[idx:idx+k]
+
+    def largestSubarray2(self, nums: List[int], k: int) -> List[int]:
         start = 0
         for i in range(1, len(nums) - k + 1):
             print(i)
