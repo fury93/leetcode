d0e4d8ea0d53535363c5090898fdc532a9cd77e3
fury93 <www.fanreala@gmail.com>
2023-12-01 09:27:27 +0300

    Update file 01/12/2023, 09:27:27

--- 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.py
+++ 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.py
@@ -1,11 +1,9 @@
 class Solution:
-    def arrayStringsAreEqual(self, word1: List
-[str], word2: List[str]) -> bool:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         def getChar(words):
             for word in words:
                 for char in word:
                     yield char
             yield None
             
-        return all(c1 == c2 for c1, c2 in zip
-(getChar(word1), getChar(word2)))
+        return all(c1 == c2 for c1, c2 in zip(getChar(word1), getChar(word2)))
