e2b3a467e6a1e4d7ee82cc8a60324d627792c628
fury93 <www.fanreala@gmail.com>
2023-12-02 23:44:34 +0300

    Update file 02/12/2023, 23:44:34

--- 1427-PerformStringShifts/1427-PerformStringShifts.py
+++ 1427-PerformStringShifts/1427-PerformStringShifts.py
@@ -1,14 +1,11 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List
+[int]]) -> str:
         shiftRes = 0
         for direction, amount in shift:
-            if direction == 0:
-                shiftRes -= amount
-            else:
-                shiftRes += amount
+            if direction == 0: shiftRes += amount
+            else: shiftRes -= amount
         shiftRes = shiftRes % len(s)
-
-        if shiftRes < 0:
-            shiftRes = abs(shiftRes)
-            return s[shiftRes:] + s[:shiftRes]
-        else:
-            return s[-shiftRes:] + s[:-shiftRes]
+        
+        return s[shiftRes:] + s[:shiftRes]
 
