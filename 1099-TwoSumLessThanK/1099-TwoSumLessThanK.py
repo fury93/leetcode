9c282b2f5dcbfcb67bd1c8f46a7f4e683aa59bc1
fury93 <www.fanreala@gmail.com>
2023-12-02 15:39:00 +0300

    Update file 02/12/2023, 15:38:59

--- 1099-TwoSumLessThanK/1099-TwoSumLessThanK.py
+++ 1099-TwoSumLessThanK/1099-TwoSumLessThanK.py
@@ -1,13 +1,6 @@
 class Solution:
-    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
-        nums = sorted(filter(lambda x: x < k, nums))
-
-        res, l, r = -1, 0, len(nums) - 1
-        while l < r:
-            sm = nums[l] + nums[r]
-            if sm >= k:
-                r -= 1
-            elif sm < k:
-                l += 1
-                res = max(res, sm)
-        return res
+    def isMajorityElement(self, nums: List[int]
+, target: int) -> bool:
+        return bisect_right(nums, target) - 
+bisect.bisect_left(nums, target) >= len(nums)//
+2 + 1
