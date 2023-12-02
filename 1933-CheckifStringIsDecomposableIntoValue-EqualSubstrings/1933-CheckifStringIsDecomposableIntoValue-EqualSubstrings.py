548eb80c8502760e012e9b88a06cbf2ff1c6f1c2
fury93 <www.fanreala@gmail.com>
2023-12-02 22:06:30 +0300

    Update file 02/12/2023, 22:06:30

--- 1933-CheckifStringIsDecomposableIntoValue-EqualSubstrings/1933-CheckifStringIsDecomposableIntoValue-EqualSubstrings.py
+++ 1933-CheckifStringIsDecomposableIntoValue-EqualSubstrings/1933-CheckifStringIsDecomposableIntoValue-EqualSubstrings.py
@@ -1,10 +1,10 @@
 class Solution:
     def isDecomposable(self, s: str) -> bool:
-        ifTwoReserved = False
+        isMetTwo = False
         for _, gr in groupby(s):
             ln = len(list(gr))
             if ln % 3 == 0: continue
-            if ln % 3 != 2 or ifTwoReserved: return False
-            ifTwoReserved = True
-        return ifTwoReserved
+            if ln % 3 != 2 or isMetTwo: return False
+            isMetTwo = True
+        return isMetTwo
 
