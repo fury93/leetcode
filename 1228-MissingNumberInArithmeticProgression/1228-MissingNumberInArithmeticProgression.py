e6d9bce6395400bf79cbea9000c43061e35b9521
fury93 <www.fanreala@gmail.com>
2023-12-02 22:29:24 +0300

    Update file 02/12/2023, 22:29:23

--- 1228-MissingNumberInArithmeticProgression/1228-MissingNumberInArithmeticProgression.py
+++ 1228-MissingNumberInArithmeticProgression/1228-MissingNumberInArithmeticProgression.py
@@ -1,16 +1,10 @@
+[
 class Solution:
-    def transformArray(self, arr: List[int]) -> List[int]:
-        hasChanges = True
-        while hasChanges:
-            hasChanges = False
-            resArr = arr.copy()
-            for i in range(1, len(arr) - 1):
-                if arr[i-1] < arr[i] > arr[i+1]:
-                    resArr[i] -= 1
-                    hasChanges = True
-                elif arr[i-1] > arr[i] < arr[i+1]:
-                    resArr[i] += 1
-                    hasChanges = True
-            arr = resArr
-        return arr
-            
+    def missingNumber(self, arr: List[int]) -> int:
+        minVal, maxVal, L = min(arr), max(arr), len(arr)
+        if minVal == maxVal: return minVal
+        step = (maxVal - minVal) // L
+        arrSet = set(arr)
+        for n in range(minVal, maxVal+1, step):
+            if n not in arrSet:
+                return n
