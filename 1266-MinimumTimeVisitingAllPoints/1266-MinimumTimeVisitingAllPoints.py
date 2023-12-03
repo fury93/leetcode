f22b2aa81d8b18099b9109fd22e48a0e2d6fdc17
fury93 <www.fanreala@gmail.com>
2023-12-03 22:57:27 +0300

    Update file 03/12/2023, 22:57:27

--- 1266-MinimumTimeVisitingAllPoints/1266-MinimumTimeVisitingAllPoints.py
+++ 1266-MinimumTimeVisitingAllPoints/1266-MinimumTimeVisitingAllPoints.py
@@ -1,9 +1,17 @@
-class Solution:
-    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        res = 0
-        for i in range(1, len(points)):
-            prev = points[i-1]
-            cur = points[i]
-            res += max(abs(cur[0] - prev[0]), abs(cur[1] - prev[1]))
-        
-        return res
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank
+[rootY]:
