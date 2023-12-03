6e7522310b8daa05d50f0c73acda7623bc0b7817
fury93 <www.fanreala@gmail.com>
2023-12-03 21:21:44 +0300

    Update file 03/12/2023, 21:21:43

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,16 +1,26 @@
 [
-class Solution:
-    def findCircleNum(self, A):
-        
-        def dfs(node):
-            for city, isConnected in enumerate(A[node]):
-                if isConnected and city not in seen:
-                    seen.add(city)
-                    dfs(city)
-        
-        ans, seen = 0, set()
-        for city in range(len(A)):
-            if city not in seen:
-                dfs(city)
-                ans += 1
-        return ans
+class UnionFind:
+    def __init__(self, n: int): 
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, node) -> int:
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, node1, node2) -> int:
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+        if root1 == root2: return False
+
+        if self.rank[root1] > self.rank[root2]:
+            self.parent[root2] = root2
+        elif self.rank[root1] < self.rank[root2]:
+            self.parent[root1] = root2
+        else:
+            self.parent[root1] = root2
+            self.rank[root1] += 1
+        return True
+    
