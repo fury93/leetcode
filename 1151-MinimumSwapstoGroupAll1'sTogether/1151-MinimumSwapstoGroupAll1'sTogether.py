d14698633366c4896052f95c31838aff29fc2487
fury93 <www.fanreala@gmail.com>
2023-12-04 23:44:22 +0300

    Update file 04/12/2023, 23:44:22

--- 1151-MinimumSwapstoGroupAll1'sTogether/1151-MinimumSwapstoGroupAll1'sTogether.py
+++ 1151-MinimumSwapstoGroupAll1'sTogether/1151-MinimumSwapstoGroupAll1'sTogether.py
@@ -1,17 +1,19 @@
 [
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
+        ones = sum(data)
+        if ones == 0: return 0
+        res = zeros = ones - sum(data[:ones])
+        for i in range(ones, len(data)):
+            zeros += data[i] == 0
+            zeros -= data[i-ones] == 0
+            res = min(res, zeros)
+        return res
+
+    def minSwaps2(self, data: List[int]) -> int:
         res, zeros, ones = math.inf, 0, sum(data)
         if not ones: return 0
 
         for r, n in enumerate(data, start=1):
             zeros += n == 0
-            if r < ones: continue
-            res = min(res, zeros)
-            zeros -= data[r - ones] == 0
-        
-        return res
-
-
-
 1
