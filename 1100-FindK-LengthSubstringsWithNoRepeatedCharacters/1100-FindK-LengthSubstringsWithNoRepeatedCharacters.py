4943cb77a7039718205d1f7a2a22c569bedc599b
fury93 <www.fanreala@gmail.com>
2023-12-04 21:08:00 +0300

    Update file 04/12/2023, 21:08:00

--- 1100-FindK-LengthSubstringsWithNoRepeatedCharacters/1100-FindK-LengthSubstringsWithNoRepeatedCharacters.py
+++ 1100-FindK-LengthSubstringsWithNoRepeatedCharacters/1100-FindK-LengthSubstringsWithNoRepeatedCharacters.py
@@ -2,14 +2,11 @@ class Solution:
     def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
         res, seen, l = 0, set(), 0
         for r, ch in enumerate(s):
-            while ch in seen:
+            while ch in seen or r - l + 1 > k:
                 seen.discard(s[l])
                 l += 1
             seen.add(ch)
-
-            if len(seen) == k:
-                res += 1
-                seen.discard(s[l])
-                l += 1
+            
+            if len(seen) == k: res += 1
             
         return res
