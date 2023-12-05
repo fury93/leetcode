3ccbe7f4508e5cbc2425f659fdc707f40d71d3ed
fury93 <www.fanreala@gmail.com>
2023-12-05 19:37:13 +0300

    Update file 05/12/2023, 19:37:12

--- 161-OneEditDistance/161-OneEditDistance.py
+++ 161-OneEditDistance/161-OneEditDistance.py
@@ -1,24 +1,25 @@
 "
-        # mode 1 - insert,  2-remove, 3 - change
-        if l1 < l2: mode = 1
-        elif l1 > l2: mode = 2
-        else: mode = 3
-
-        i, j, isEdited = 0, 0, False
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        l1, l2 = len(s), len(t)
+        if abs(l1 - l2) > 1: return False
+        
+        # mode {1: insert, -1: remove, 0: change}
+        mode, i, j, isEdited = l2 - l1, 0, 0, False
 
         while i < l1 or j < l2:
             ss = s[i] if i < l1 else None
             tt = t[j] if j < l2 else None
-            if ss != tt:
+            if ss == tt:
+                i, j = i + 1, j + 1
+            else:
                 if isEdited: return False
                 isEdited = True
                 if mode == 1:
                     j += 1
-                    continue
-                elif mode == 2:
+                elif mode == -1:
                     i += 1
-                    continue
-            i += 1
-            j += 1
+                else:
+                    i, j = i + 1, j + 1
         
         return isEdited
