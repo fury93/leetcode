072cdba08494b26819f16ac119785d6f31ab658e
fury93 <www.fanreala@gmail.com>
2023-12-06 22:13:07 +0300

    Create file

---
+++ 1874-MinimizeProductSumofTwoArrays/1874-MinimizeProductSumofTwoArrays.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minProductSum(self, nums1: List
+[int], nums2: List[int]) -> int:
+        return sum(i * j for i, j in zip
+(sorted(nums1), sorted(nums2, 
+reverse=True)))
+        
