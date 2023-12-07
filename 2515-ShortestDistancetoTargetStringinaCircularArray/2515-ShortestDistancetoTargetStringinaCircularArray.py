d437c1488e0e6041621fbe9916027f0ca7ce878b
fury93 <www.fanreala@gmail.com>
2023-12-07 21:10:52 +0300

    Update file 07/12/2023, 21:10:52

--- 2515-ShortestDistancetoTargetStringinaCircularArray/2515-ShortestDistancetoTargetStringinaCircularArray.py
+++ 2515-ShortestDistancetoTargetStringinaCircularArray/2515-ShortestDistancetoTargetStringinaCircularArray.py
@@ -1,10 +1,13 @@
 [
 class Solution:
-    def closetTarget(self, words: List[str], target: str, start: int) -> int:
+    def closetTarget(self, words: List[str], 
+target: str, start: int) -> int:
         ln = len(words)
         for k in range(ln//2 + 1):
-            l, r = start - k, start + k + 1
-            if words[l % ln] == target: return abs(start - l)
-            if words[r % ln] == target: return abs(r - start)
+            l, r = start - k, start + k
+            if words[l % ln] == target: return 
+abs(start - l)
+            if words[r % ln] == target: return 
+abs(r - start)
         return -1
 5
