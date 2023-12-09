24358a85b61a10dd136fb53254b1b64cc0f6d89b
fury93 <www.fanreala@gmail.com>
2023-12-09 21:22:04 +0300

    Create file

---
+++ Explore/Explore.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional
+[TreeNode]:
+        pos = {val: idx for idx, val in enumerate(inorder)}
+        self.idx = 0
+
+        def dfs(l, r):
+            if self.idx == len(preorder) or l > r: return None
+            root = preorder[self.idx]
+            self.idx += 1
+            return TreeNode(
+                root,
+                dfs(l, pos[root]-1),
+                dfs(pos[root] + 1, r)
+            )
+
+        return dfs(0, len(inorder) - 1)
+
+    def buildTree2(self, preorder: List[int], inorder: List[int]) -> Optional
