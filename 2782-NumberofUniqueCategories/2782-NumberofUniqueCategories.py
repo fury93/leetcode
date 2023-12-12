ec00c413ca0dacc45870011bfaf7e0dc8442b6e1
fury93 <www.fanreala@gmail.com>
2023-12-13 02:01:00 +0300

    Create file

---
+++ 2782-NumberofUniqueCategories/2782-NumberofUniqueCategories.py
@@ -0,0 +1,17 @@
+# Definition for a category handler.
+# class CategoryHandler:
+#     def haveSameCategory(self, a: int, b: int) -> bool:
+#         pass
+class Solution:
+    def numberOfCategories(self, n: int, categoryHandler: Optional
+['CategoryHandler']) -> int:
+        res = n
+        for i in range(n):
+            for j in range(i+1, n):
+                if categoryHandler.haveSameCategory(i, j):
+                    res -= 1
+                    break
+
+        return res
+
+
