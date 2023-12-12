f817163a54ebd897a01573cfcce38f88961c511b
fury93 <www.fanreala@gmail.com>
2023-12-13 02:01:49 +0300

    Update file 13/12/2023, 02:01:49

--- 2782-NumberofUniqueCategories/2782-NumberofUniqueCategories.py
+++ 2782-NumberofUniqueCategories/2782-NumberofUniqueCategories.py
@@ -3,11 +3,10 @@
 #     def haveSameCategory(self, a: int, b: int) -> bool:
 #         pass
 class Solution:
-    def numberOfCategories(self, n: int, categoryHandler: Optional
-['CategoryHandler']) -> int:
+    def numberOfCategories(self, n: int, categoryHandler: Optional['CategoryHandler']) -> int:
         res = n
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i):
                 if categoryHandler.haveSameCategory(i, j):
                     res -= 1
                     break
