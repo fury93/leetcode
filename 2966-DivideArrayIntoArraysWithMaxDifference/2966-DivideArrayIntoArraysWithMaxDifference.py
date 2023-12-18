67c345b7d61b736c61688f99264226e57f3a50f6
fury93 <www.fanreala@gmail.com>
2023-12-19 00:30:21 +0300

    Update file 19/12/2023, 00:30:21

--- 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.py
+++ 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.py
@@ -1,15 +1,12 @@
 class Solution:
     def findMissingAndRepeatedValues(self, 
 grid: List[List[int]]) -> List[int]:
-        vals, dup = set(), None
+        vals, dup, mis = set(), None, 1
         for val in chain(*grid):
-            if not dup and val in vals:
-                dup = val
+            if val in vals: dup = val
             vals.add(val)
         
-        mis = 1
-        while mis in vals:
-            mis += 1
+        while mis in vals: mis += 1
                 
         return [dup, mis]
 
