423e00e980d15a3038f3ca07641c55756ef52492
fury93 <www.fanreala@gmail.com>
2023-12-26 03:11:39 +0300

    Create file

---
+++ 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRollsToTarget(self, d: int, f: int, target: int) -> int:
+        dp = [[0 for i in range(target + 1)] for j in range(d + 1)]
+        dp[0][0] = 1
+        mod = 10 ** 9 + 7
+        for i in range(1, d + 1):
+            for j in range(1, target + 1):
+                k = 1
+                while k <= min(j, f):
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % mod
+                    k += 1
+        return dp[d][target] % mod
