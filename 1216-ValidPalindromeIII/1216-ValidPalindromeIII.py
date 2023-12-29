9b81f4e849de4dc4ccef851e870941dc6e82811a
fury93 <www.fanreala@gmail.com>
2023-12-29 04:41:12 +0300

    Update file 29/12/2023, 04:41:12

--- 1216-ValidPalindromeIII/1216-ValidPalindromeIII.py
+++ 1216-ValidPalindromeIII/1216-ValidPalindromeIII.py
@@ -1,16 +1,16 @@
+"
 class Solution:
-    def isValidPalindrome(self, s: str, k: int) 
--> bool:
-        n = len(s)
-        dp = [[0] * (n + 1) for _ in range(n + 1)
-] 
-        for i in range(n + 1): 
-            for j in range(n + 1): 
-                if not i or not j: 
-                    dp[i][j] = i or j 
-                elif s[i - 1] == s[n - j]: 
-                    dp[i][j] = dp[i - 1][j - 1] 
-                else: 
-                    dp[i][j] = 1 + min(dp[i - 1]
-[j], dp[i][j - 1])
-        return dp[n][n] <= k * 2
+    def makePalindrome(self, s: str) -> bool:
+        swapsAllowed, l, r = 2, 0, len(s) - 1
+        while l < r:
+            if s[l] == s[r]:
+                l, r = l + 1, r - 1
+            elif swapsAllowed > 0:
+                swapsAllowed -= 1
+                l, r = l + 1, r - 1
+            else: return False
+
+        return swapsAllowed <= 2
+
+
+        
