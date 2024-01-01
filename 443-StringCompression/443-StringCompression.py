f12edf25168b25c7749e4203c1c75d07a9a3b00d
fury93 <www.fanreala@gmail.com>
2024-01-01 21:08:44 +0300

    Update file 01/01/2024, 21:08:43

--- 443-StringCompression/443-StringCompression.py
+++ 443-StringCompression/443-StringCompression.py
@@ -11,8 +11,8 @@ class Solution:
             chars[i] = char
             i += 1
             if cnt > 1:
-                for d in str(cnt):
-                    chars[i] = d
-                    i += 1
+                cnt_str = str(cnt)
+                start, i = i, i + len(cnt_str)
+                chars[start:i] = list(cnt_str)
 
         return i
