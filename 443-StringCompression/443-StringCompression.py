899bd6e2192b017348a3096b55b6ee783afda065
fury93 <www.fanreala@gmail.com>
2024-01-01 21:30:18 +0300

    Update file 01/01/2024, 21:30:17

--- 443-StringCompression/443-StringCompression.py
+++ 443-StringCompression/443-StringCompression.py
@@ -5,15 +5,14 @@ class RLEIterator:
         self.p = 0
 
     def next(self, n: int) -> int:
-        while n > 0 and self.p < len(self.s):
+        while self.p < len(self.s):
             if self.s[self.p] >= n:
                 self.s[self.p] -= n
-                break
-            else:
-                n -= self.s[self.p]
-                self.p += 2
+                return  self.s[self.p+1]
+            n -= self.s[self.p]
+            self.p += 2
         
-        return self.s[self.p+1] if self.p < len(self.s) else - 1
+        return -1
         
 
 
