8239365e3a28763693560ce1496aa5bf6fa30f0b
fury93 <www.fanreala@gmail.com>
2024-01-01 06:42:08 +0300

    Create file

---
+++ 455-AssignCookies/455-AssignCookies.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        res, i, j = 0, 0, 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                res += 1
+                i += 1
+            j +=1
+        return res
