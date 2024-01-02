5e128934b99ae85a19684b1a87a776d4855a232c
fury93 <www.fanreala@gmail.com>
2024-01-02 20:05:05 +0300

    Create file

---
+++ 341-FlattenNestedListIterator/341-FlattenNestedListIterator.py
@@ -0,0 +1,16 @@
+# """
+# This is the interface that allows for 
+creating nested lists.
+# You should not implement it, or 
+speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this 
+NestedInteger holds a single integer, 
+rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
