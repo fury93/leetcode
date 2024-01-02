5fea6e10a165ed44800f4e9848dc89a8e7c70b27
fury93 <www.fanreala@gmail.com>
2024-01-02 19:55:41 +0300

    Update file 02/01/2024, 19:55:40

--- 251-Flatten2DVector/251-Flatten2DVector.py
+++ 251-Flatten2DVector/251-Flatten2DVector.py
@@ -6,17 +6,11 @@ class Vector2D:
         self.inner = 0
 
     def __moveToNext(self):
-        while self.outer < len(self.vec) and len(self.vec[self.outer]) == 
-self.inner:
+        while self.outer < len(self.vec) and len
+(self.vec[self.outer]) == self.inner:
             self.outer += 1
             self.inner = 0
 
     def next(self) -> int:
-        self.__moveToNext()
-        val = self.vec[self.outer][self.inner]
-        self.inner += 1
-        return val
-        
-    def hasNext(self) -> bool:
-        self.__moveToNext()
-        return self.outer < len(self.vec)
+        if not self.hasNext():
+            raise StopIteration
