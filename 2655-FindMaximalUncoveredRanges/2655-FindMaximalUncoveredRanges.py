460e1c00ac62e5e2882289a644f580982c6dafea
fury93 <www.fanreala@gmail.com>
2024-01-03 22:07:07 +0300

    Update file 03/01/2024, 22:07:07

--- 2655-FindMaximalUncoveredRanges/2655-FindMaximalUncoveredRanges.py
+++ 2655-FindMaximalUncoveredRanges/2655-FindMaximalUncoveredRanges.py
@@ -6,7 +6,7 @@ class Solution:
         for start, end in ranges + [[n , n]]:
             if missing_start < start:
                 res.append([missing_start, start-1])
-            missing_start = max(missing_start+1, end + 1)
+            missing_start = max(missing_start, end + 1)
         return res 
 
     def findMaximalUncoveredRanges2(self, n: int, ranges: List[List[int]]) -> List[List[int]]:
