84a49381a19a654c5ba1ff31c16f647c13496e8d
fury93 <www.fanreala@gmail.com>
2024-01-03 23:34:25 +0300

    Update file 03/01/2024, 23:34:24

--- 163-MissingRanges/163-MissingRanges.py
+++ 163-MissingRanges/163-MissingRanges.py
@@ -1,28 +1,11 @@
-[
 class Solution:
-    def findMissingRanges2(self, nums: List[int], lower: int, upper: int) 
--> List[List[int]]:
-        res = []
-        for n in chain(nums, [upper+1]):
-            if lower != n:
-                res.append([lower, n-1])
-            lower = n + 1
-        return res
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        ranges.sort()
+        for start, end in ranges:
+            if end < left: continue #skip all intervals < left
 
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) 
--> List[List[int]]:
-        if not nums: return[[lower, upper]]
-        
-        if nums[0] != lower:
-            nums.insert(0, lower-1)
-        if nums[-1] != upper:
-            nums.append(upper+1)
-        
-        res = []
-        for i in range(len(nums) - 1):
-            start, end = nums[i], nums[i+1]
-            if start + 1 != end:
-                res.append([start+1, end-1])
-        
-        #for start, end in zip(nums, nums[1:]):
-        #    if start + 1 != end:
+            if start > left: return False # don't cover diapason [left:start-1]
+            left = max(left + 1, end + 1)
+            if left-1 >= right: return True
+
+        return False
