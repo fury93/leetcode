bb46dbc22c063f944015855b4f43e632456b4200
fury93 <www.fanreala@gmail.com>
2024-01-03 20:13:37 +0300

    Create file

---
+++ 163-MissingRanges/163-MissingRanges.py
@@ -0,0 +1,25 @@
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        res = []
+        for n in chain(nums, [upper+1]):
+            if lower != n:
+                res.append([lower, n-1])
+            lower = n + 1
+        return res
+
+    def findMissingRanges2(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        res = []
+        if not nums:
+            nums = [lower-1, upper+1]
+        else:
+            if nums[0] != lower:
+                nums.insert(0, lower-1)
+            if nums[-1] != upper:
+                nums.append(upper+1)
+        
+        for start, end in zip(nums, nums[1:]):
+            if start + 1 != end:
+                res.append([start+1, end-1])
+        
+        return res
+
+class Solution:
