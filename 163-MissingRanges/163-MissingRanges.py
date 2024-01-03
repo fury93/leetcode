871ff7d61ae60d10c228c639f74b82090e2c074d
fury93 <www.fanreala@gmail.com>
2024-01-04 00:54:32 +0300

    Update file 04/01/2024, 00:54:32

--- 163-MissingRanges/163-MissingRanges.py
+++ 163-MissingRanges/163-MissingRanges.py
@@ -3,7 +3,6 @@ class Solution:
         ranges.sort()
         for start, end in ranges:
             if end < left: continue #skip all intervals < left
-
             if start > left: return False # don't cover diapason [left:start-1]
             left = max(left + 1, end + 1)
             if left-1 >= right: return True
