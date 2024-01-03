dac02f8e9104fb0a41904f2bd8bb3d0516cae48a
fury93 <www.fanreala@gmail.com>
2024-01-03 21:58:49 +0300

    Update file 03/01/2024, 21:58:48

--- 2655-FindMaximalUncoveredRanges/2655-FindMaximalUncoveredRanges.py
+++ 2655-FindMaximalUncoveredRanges/2655-FindMaximalUncoveredRanges.py
@@ -1,17 +1,24 @@
 1
 class Solution:
     def findMaximalUncoveredRanges(self, n: int, ranges: List[List[int]]) -> List[List[int]]:
-        if not ranges:
-            return [[0, n-1]]
-        
-        # sort by start and merge intervals
         ranges.sort()
-        merged_ranges = []
-        for start, end in ranges:
-            if merged_ranges and merged_ranges[-1][1] >= start:
-                merged_ranges[-1][1] = max(merged_ranges[-1][1], end)
+        res, missing_start = [], 0
+        for start, end in ranges + [[n , n]]:
+            if missing_start < start:
+                res.append([missing_start, start-1])
+            missing_start = max(missing_start+1, end + 1)
+        return res 
+
+    def findMaximalUncoveredRanges2(self, n: int, ranges: List[List[int]]) -> List[List[int]]:
+        if not ranges: return [[0, n-1]]
+        
+        ranges.sort()
+        merged_ranges = [ranges[0]]
+        for i in range(1, len(ranges)):
+            if merged_ranges[-1][1] >= ranges[i][0]:
+                merged_ranges[-1][1] = max(merged_ranges[-1][1], ranges[i][1])
             else:
-                merged_ranges.append([start, end])
+                merged_ranges.append(ranges[i])
           
         if merged_ranges[-1][1] != n:
             merged_ranges.append([n,n])
@@ -19,6 +26,3 @@ class Solution:
         res, missing_start = [], 0
         for start, end in merged_ranges:
             if missing_start != start:
-                res.append([missing_start, start-1])
-            missing_start = end + 1
-        return res      
