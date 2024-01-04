256376bb6adddd91d11af332876850daae80b021
fury93 <www.fanreala@gmail.com>
2024-01-04 03:02:17 +0300

    Update file 04/01/2024, 03:02:16

--- 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.py
+++ 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.py
@@ -1,6 +1,5 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         val = Counter(nums).values()
-        return -1 if min(val) == 1 else sum((v + 2) // 3 for v 
-in val)
+        return -1 if min(val) == 1 else sum((v + 2) // 3 for v in val)
         
