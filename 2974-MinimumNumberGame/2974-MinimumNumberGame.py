ebd162e3c8f0d9206b9a78a6e013faaf77f8a700
fury93 <www.fanreala@gmail.com>
2024-01-06 02:29:37 +0300

    Update file 06/01/2024, 02:29:37

--- 2974-MinimumNumberGame/2974-MinimumNumberGame.py
+++ 2974-MinimumNumberGame/2974-MinimumNumberGame.py
@@ -1,7 +1,6 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
         nums.sort()
-        res = []
         for i in range(0, len(nums), 2):
-            res.extend([nums[i+1],·nums[i]])
-        return res
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+        return nums
