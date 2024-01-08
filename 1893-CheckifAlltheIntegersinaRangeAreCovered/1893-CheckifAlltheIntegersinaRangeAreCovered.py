11253bd7c90731444d7963066497c6f1a934f458
fury93 <www.fanreala@gmail.com>
2024-01-08 22:17:22 +0300

    Update file 08/01/2024, 22:17:21

--- 1893-CheckifAlltheIntegersinaRangeAreCovered/1893-CheckifAlltheIntegersinaRangeAreCovered.py
+++ 1893-CheckifAlltheIntegersinaRangeAreCovered/1893-CheckifAlltheIntegersinaRangeAreCovered.py
@@ -1,10 +1,11 @@
 class Solution:
-    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) 
+-> bool:
         ranges.sort()
         for start, end in ranges:
-            if end < left: continue #skip all intervals < left
-            if start > left: return False # don't cover diapason [left:start-1]
-            left = end + 1
-            if left-1 >= right: return True
+            if end < left: continue
+            if start > left: return False
+            if end >= right: return True
 
+            left = end + 1
         return False
