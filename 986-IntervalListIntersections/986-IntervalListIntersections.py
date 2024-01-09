082934bf842fc3f1936ed1cb7bccf0fbf9bd1949
fury93 <www.fanreala@gmail.com>
2024-01-10 01:31:00 +0300

    Update file 10/01/2024, 01:31:00

--- 986-IntervalListIntersections/986-IntervalListIntersections.py
+++ 986-IntervalListIntersections/986-IntervalListIntersections.py
@@ -1,17 +1,9 @@
+[
 class Solution:
-    def intervalIntersection(self, A: List
-[List[int]], B: List[List[int]]) -> List
-[List[int]]:
-        res, i, j = [], 0, 0
-        while i < len(A) and j < len(B):
-            start, end = max(A[i][0], B[j]
-[0]), min(A[i][1], B[j][1])
-            if start <= end:
-                res.append([start, end])
-            
-            if A[i][1] < B[j][1]:
-                i += 1
-            else:
-                j += 1
-        
-        return res
+    def isRectangleOverlap(self, rec1: List[int], rec2: 
+List[int]) -> bool:
+        x1_1,y1_1,x2_1,y2_1 = rec1
+        x1_2,y1_2,x2_2,y2_2 = rec2
+        width = min(x2_1,x2_2) - max(x1_1, x1_2)
+        height = min(y2_1, y2_2) - max(y1_1, y1_2)
+        return width > 0 and height > 0
