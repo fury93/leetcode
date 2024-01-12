b9dff43ac56deac0378714713ef866e0f9e8ac69
fury93 <www.fanreala@gmail.com>
2024-01-12 07:41:01 +0300

    Update file 12/01/2024, 07:41:01

--- 370-RangeAddition/370-RangeAddition.py
+++ 370-RangeAddition/370-RangeAddition.py
@@ -1,11 +1,12 @@
-class Solution:
-    def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
+    def getModifiedArray(self, length: int, updates: List[List[int]]
+) -> List[int]:
         res = [0] * length
 
         for start, end, diff in updates:
             res[start] += diff
-            if end+1 < length:
-                res[end+1] -= diff
+            end += 1
+            if end < length:
+                res[end] -= diff
         
         return accumulate(res)
 5
