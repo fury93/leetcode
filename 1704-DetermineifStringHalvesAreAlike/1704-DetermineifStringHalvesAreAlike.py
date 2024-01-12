fde39836de0ba04fc9d6df8fe0510167a39c86d3
fury93 <www.fanreala@gmail.com>
2024-01-12 06:50:11 +0300

    Update file 12/01/2024, 06:50:11

--- 1704-DetermineifStringHalvesAreAlike/1704-DetermineifStringHalvesAreAlike.py
+++ 1704-DetermineifStringHalvesAreAlike/1704-DetermineifStringHalvesAreAlike.py
@@ -1,6 +1,6 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        cnt, vowels = 0, 'a,e,i,o,u,A,E,I,O,U'
+        cnt, vowels = 0, set('a,e,i,o,u,A,E,I,O,U')
         
         for i in range(len(s)//2):
             cnt += s[i] in vowels
