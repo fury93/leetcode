51601fa674fdee5b184685e7eafd4b1b7de03141
fury93 <www.fanreala@gmail.com>
2024-01-12 06:11:56 +0300

    Update file 12/01/2024, 06:11:56

--- 1094-CarPooling/1094-CarPooling.py
+++ 1094-CarPooling/1094-CarPooling.py
@@ -1,5 +1,17 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        locations = []
+        for numPassengers, start, end in trips:
+            locations.extend([(start, numPassengers), (end, -numPassengers)])
+        locations.sort()
+
+        for _, numPassengers in locations:
+            capacity -= numPassengers
+            if capacity < 0: return False
+
+        return True
+
+    def carPooling2(self, trips: List[List[int]], capacity: int) -> bool:
         heap = []
         for numPassengers, start, end in trips:
             heap.extend([(start, numPassengers), (end, -numPassengers)])
