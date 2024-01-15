43e2baa1d991f481161c5cafb5a7a0a2430a8428
fury93 <www.fanreala@gmail.com>
2024-01-15 21:19:56 +0300

    Create file

---
+++ 2758-NextDay/2758-NextDay.py
@@ -0,0 +1,10 @@
+Date.prototype.nextDay = function() {
+    const next = this.getTime() + 24 * 60 * 60 * 1000;
+    return new Date(next).toISOString().split("T")[0];
+}
+
+/**
+ * const date = new Date("2014-06-20");
+ * date.nextDay(); // "2014-06-21"
+ */
+"
