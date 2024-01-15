fd14b8e7bb25315941d861fb1c37eb838e319b3f
fury93 <www.fanreala@gmail.com>
2024-01-15 21:31:43 +0300

    Create file

---
+++ 2725-IntervalCancellation/2725-IntervalCancellation.py
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    const timer = setInterval(() => fn(...args), t);
+
+    const cancelFn = () => clearInterval(timer);
+    return cancelFn;
+};
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+(
