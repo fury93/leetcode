ddd3550a6f08c0e4d497bd2a0430abbd33f78145
fury93 <www.fanreala@gmail.com>
2024-01-16 00:21:34 +0300

    Create file

---
+++ 2777-DateRangeGenerator/2777-DateRangeGenerator.py
@@ -0,0 +1,18 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {number} step
+ * @yields {string}
+ */
+var dateRangeGenerator = function* (start, end, step) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    while (startDate <= endDate) {
+        yield startDate.toISOString().split('T')[0].trim();
+        startDate.setDate(startDate.getDate() + step);
+    }
+};
+"2023-04-01"
+"2023-04-04"
+1
