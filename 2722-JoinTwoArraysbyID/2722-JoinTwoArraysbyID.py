50fa7cfb253a3627113fbcae35376638d0652451
fury93 <www.fanreala@gmail.com>
2024-01-16 00:19:24 +0300

    Update file 16/01/2024, 00:19:23

--- 2722-JoinTwoArraysbyID/2722-JoinTwoArraysbyID.py
+++ 2722-JoinTwoArraysbyID/2722-JoinTwoArraysbyID.py
@@ -1,12 +1,23 @@
-
-Function.prototype.bindPolyfill = function(obj) {
-  const self = this;  // Reference to the original function using closure
-
-  return function(...args) {     // Return a new function
-    return self.apply(obj, args);  // Invoke original function with obj context using apply
+    return obj2;
   }
-}
 
-function f(multiplier) { return this.x * multiplier; }
-{"x":10}
-[5]
+  // Use the first object as the base for merging
+  const res = obj1;
+
+  // Iterate through the properties of obj2
+  for (const key in obj2) {
+    if (key in res) {
+      // If the property exists in both, recursively merge them
+  if (obj1 === null || obj2 === null) {
+  // If either input is null, return obj2
+  if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || Array.isArray(obj1) !== Array.
+isArray(obj2)) {
+    return obj2;
+  }
+
+
+var deepMerge = function(obj1, obj2) {
+  // If either input is not an object or their types differ (array vs. object), return obj2
+{"a": 1, "c": 3}
+{"a": 2, "b": 2}
+[{}, 2, 3]
