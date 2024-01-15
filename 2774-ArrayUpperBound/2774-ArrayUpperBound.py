7ca71844d3ed21971db3e104ae52ebf19c4bc732
fury93 <www.fanreala@gmail.com>
2024-01-15 21:26:44 +0300

    Update file 15/01/2024, 21:26:44

--- 2774-ArrayUpperBound/2774-ArrayUpperBound.py
+++ 2774-ArrayUpperBound/2774-ArrayUpperBound.py
@@ -1,15 +1,8 @@
-/** 
- * @param {number} target
- * @return {number}
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
  */
-Array.prototype.upperBound = function(target) {
-    return this.lastIndexOf(target)
+var isEmpty = function(obj) {
+  return Object.keys(obj).length === 0
 };
-
-
-// [3,4,5].upperBound(5); // 2
-// [1,4,5].upperBound(2); // -1
-// [3,4,6,6,6,6,7].upperBound(6) // 5
-[3,4,5]
-5
-[1,4,5]
+{
