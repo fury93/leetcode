0c3bd289bdb5fc6709667d420f7696ce0fa78d31
fury93 <www.fanreala@gmail.com>
2024-01-16 00:20:42 +0300

    Update file 16/01/2024, 00:20:41

--- 2759-ConvertJSONStringtoObject/2759-ConvertJSONStringtoObject.py
+++ 2759-ConvertJSONStringtoObject/2759-ConvertJSONStringtoObject.py
@@ -1,23 +1,13 @@
-            const newStruct = str[i] === '[' ? [] : {};
-
-            // If this is the first structure, set it as root.
-            if (root === null) root = newStruct;
-
-            if (currentStruct !== null) {
-   let currentKey = null;  // Key for the current object value being processed.
-
-   for(let i = 0; i < length; i++){
-      if(str[i] === ",") continue;  // Skip commas.
-
-      switch(str[i]) {
-         case '[':
-         case '{':
-var jsonParse = function(str) {
-   const length = str.length;
-   const stack = [];  // Stack to maintain the hierarchy of nested structures.
-   let currentStruct = null;  // Current structure being processed (either an array or an 
-object).
-   let root = null;  // Root structure of the parsed JSON.
-'{"a":2,"b":[1,2,3]}'
-'true'
-'[1,5,"false",{"a":2}]'
+var undefinedToNull = function(obj) {
+    if (obj === null || obj === undefined) return null;
+    if (typeof obj !== 'object') return obj;
+    if (Array.isArray(obj)) return obj.map(undefinedToNull);
+    
+    let res = {};
+    for (let key in obj) {
+        res[key] = undefinedToNull(obj[key]);
+    }
+    return res
+};
+{"a": undefined, "b":3}
+{"a": undefined, "b":["a", undefined]}
