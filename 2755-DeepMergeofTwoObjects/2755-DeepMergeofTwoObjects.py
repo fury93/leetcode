abdfca18433873ceceafe005790dc9701dc7a5bb
fury93 <www.fanreala@gmail.com>
2024-01-16 00:18:24 +0300

    Create file

---
+++ 2755-DeepMergeofTwoObjects/2755-DeepMergeofTwoObjects.py
@@ -0,0 +1,23 @@
+          }
+
+          return newArr;
+      }
+
+      const newObj = {};
+              }
+              if(subRes) {
+                  newArr.push(subRes);
+              const subRes = dfs(curr);
+
+      if (Array.isArray(obj)) {
+          const newArr = [];
+          for(let i = 0; i < obj.length; i++) {
+              const curr = obj[i];
+var compactObject = function(obj) {
+  function dfs(obj) {
+      if (!obj) return false;
+      if (typeof obj !== 'object') return obj;
+
+[null, 0, false, 1]
+{"a": null, "b": [false, 1]}
+[null, 0, 5, [0], [false, 16]]
