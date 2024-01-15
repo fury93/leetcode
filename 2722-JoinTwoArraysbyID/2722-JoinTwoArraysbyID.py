9744dde762121ccf3fdfd72d84e76a45d860377e
fury93 <www.fanreala@gmail.com>
2024-01-16 00:19:06 +0300

    Update file 16/01/2024, 00:19:06

--- 2722-JoinTwoArraysbyID/2722-JoinTwoArraysbyID.py
+++ 2722-JoinTwoArraysbyID/2722-JoinTwoArraysbyID.py
@@ -1,22 +1,12 @@
-/**
- * @param {Array} arr1
- * @param {Array} arr2
- * @return {Array}
- */
-var join = function(arr1, arr2) {
-    const map = new Map();
-    for(const obj of arr1) map.set(obj.id, obj);
-    for(const obj of arr2) {
-        if(!map.has(obj.id)) map.set(obj.id, obj);
-        else {
-            const prevObj = map.get(obj.id);
-            for(const key of Object.keys(obj)) prevObj[key] = obj[key];
-        }
-    }
-    const res = new Array();
-    for(let key of map.keys()) res.push(map.get(key));
-    return res.sort((a,b)=>a.id-b.id); 
-};
-[{"id": 1,"x": 1},{"id": 2,"x": 9}]
-[{"id": 3,"x": 5}]
-[{"id": 1,"x": 2,"y": 3},{"id": 2,"x": 3,"y": 6}]
+
+Function.prototype.bindPolyfill = function(obj) {
+  const self = this;  // Reference to the original function using closure
+
+  return function(...args) {     // Return a new function
+    return self.apply(obj, args);  // Invoke original function with obj context using apply
+  }
+}
+
+function f(multiplier) { return this.x * multiplier; }
+{"x":10}
+[5]
