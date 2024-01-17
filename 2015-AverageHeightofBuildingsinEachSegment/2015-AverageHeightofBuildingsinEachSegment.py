09c4423fb9c864e47b3b6fb1357feaf518e296dd
fury93 <www.fanreala@gmail.com>
2024-01-17 21:34:57 +0300

    Update file 17/01/2024, 21:34:57

--- 2015-AverageHeightofBuildingsinEachSegment/2015-AverageHeightofBuildingsinEachSegment.py
+++ 2015-AverageHeightofBuildingsinEachSegment/2015-AverageHeightofBuildingsinEachSegment.py
@@ -1,6 +1,6 @@
 class Solution:
     def averageHeightOfBuildings(self, buildings: List[List[int]]) -> List[List[int]]:
-        d = defaultdict(lambda: defaultdict(int)) # (diffHeight, diffCnt)
+        d = defaultdict(lambda: [0, 0]) # (diffHeight, diffCnt)
         for start, end, height in buildings:
             d[start][0] += height
             d[start][1] += 1
@@ -14,10 +14,10 @@ class Solution:
             segmentCnt += diffCnt
             if segmentCnt > 0:
                 averageHeight = segmentHeight//segmentCnt
+                # merge with previous segment if they have the same average height and is neighbours
                 if res and res[-1][1] == start and res[-1][2] == averageHeight:
                     res[-1][1] = end
                 else:
                     res.append([start, end, averageHeight])
-                # merge with previous segment if they have the same average height and is neighbours
         return res
 [
