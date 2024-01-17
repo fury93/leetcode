2d28349574d69fea4339808287077820412e30e5
fury93 <www.fanreala@gmail.com>
2024-01-17 16:10:05 +0300

    Update file 17/01/2024, 16:10:05

--- 1272-RemoveInterval/1272-RemoveInterval.py
+++ 1272-RemoveInterval/1272-RemoveInterval.py
@@ -1,6 +1,17 @@
-class Solution:
-    def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List
-[List[int]]:
+    def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
+        res = []
+        for start, end in intervals:
+            newEnd = min(end, toBeRemoved[0])
+            newStart = max(start, toBeRemoved[1])
+            
+            if start < newEnd:
+                res.append([start, newEnd])
+            if newStart < end:
+                res.append([newStart, end])
+
+        return res
+    
+    def removeInterval2(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
         res = []
         for start, end in intervals:
             if end < toBeRemoved[0] or start > toBeRemoved[1]:
