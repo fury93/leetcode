25366653eaf426d8b818c082fb32998c47eb1ecf
fury93 <www.fanreala@gmail.com>
2024-01-17 20:07:37 +0300

    Update file 17/01/2024, 20:07:36

--- 1943-DescribethePainting/1943-DescribethePainting.py
+++ 1943-DescribethePainting/1943-DescribethePainting.py
@@ -1,3 +1,6 @@
+class Solution:
+    def splitPainting(self, segments: List[List[int]]) -> 
+List[List[int]]:
         d = defaultdict(int)
 
         for start, end, color in segments:
@@ -6,13 +9,4 @@
 
         res, color, items = [], 0, sorted(d.items())
         for i in range(len(items)-1):
-            start, colorDiff, end = items[i][0], items[i][1], items[i+1][0]
-            color += colorDiff
-            if color > 0:
-                res.append([start, end, color])
-        
-        return res
-
-    def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
-class Solution:
 [
