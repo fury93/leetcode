cbfe37173bc2a57b42b4c826f6cbdc12d42fa186
fury93 <www.fanreala@gmail.com>
2024-01-21 04:38:19 +0300

    Create file

---
+++ 732-MyCalendarIII/732-MyCalendarIII.py
@@ -0,0 +1,14 @@
+from sortedcontainers import SortedDict
+
+    def __init__(self):
+        self.diff = SortedDict()
+
+    def book(self, start: int, end: int) -> int:
+        self.diff[start] = self.diff.get(start, 0) + 1
+        self.diff[end] = self.diff.get(end, 0) - 1
+        cur = res = 0
+        for delta in self.diff.values():
+            cur += delta
+class MyCalendarThree:
+            res = max(cur, res)
+        return res
