11a119aefc2db0095563464c52ad1de6986c7831
fury93 <www.fanreala@gmail.com>
2024-01-23 07:02:18 +0300

    Update file 23/01/2024, 07:02:18

--- 1891-CuttingRibbons/1891-CuttingRibbons.py
+++ 1891-CuttingRibbons/1891-CuttingRibbons.py
@@ -1,18 +1,17 @@
-        if sm < k: return 0
-        ribbons.sort(reverse=True)
-        l, r = 1, max(ribbons)
-
         def isEnough(cutSize, k):
             for size in ribbons:
-                if size < cutSize or k == 0: break
+                #if size < cutSize or k == 0: break
                 k -= size // cutSize
-            return k <= 0
+            return False
                 
         while l < r:
             m = (l + r + 1) // 2
-            if isEnough(m, k):
-                l = m
-            else:
-                r = m - 1
-        
-        return l
+                if k <= 0: return True
+
+        l, r = 0, max(ribbons)
+        #ribbons.sort(reverse=True)
+        #if sm < k: return 0
+        #sm = sum(ribbons)
+int:
+    def maxLength(self, ribbons: List[int], k: int) -> 
+class Solution:
