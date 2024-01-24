d585926713741e8facecf3b9e91c5261665d2d97
fury93 <www.fanreala@gmail.com>
2024-01-24 22:41:07 +0300

    Update file 24/01/2024, 22:41:06

--- 1992-FindAllGroupsofFarmland/1992-FindAllGroupsofFarmland.py
+++ 1992-FindAllGroupsofFarmland/1992-FindAllGroupsofFarmland.py
@@ -1,20 +1,18 @@
-class Solution:
-    def findFarmland(self, land: List[List[int]]) -> List
-[List[int]]:
-        res, R, C = [], len(land), len(land[0])
-        
-        def dfs(x, y, mx):
-            if x < R and y < C and land[x][y] == 1:
-                land[x][y] = 0
-                for dx, dy in [(0, 1), (1, 0)]:
-                    mx = max(mx, (x, y), dfs(x + dx, y + 
-dy, mx))
-            return mx
+        def dfs(x, y):
+            maxRightDown = (x, y)
+            land[x][y] = 0
+            for dx, dy in [(0, 1), (1, 0)]:
+                xx, yy = x + dx, y + dy
+                if xx < R and yy < C and land[xx][yy] == 1:
+                    maxRightDown = max(dfs(xx, yy), maxRightDown)
+            return maxRightDown 
 
-        for x, y in itertools.product(range(R), range(C))
-:
+        for x, y in itertools.product(range(R), range(C)):
             if land[x][y] == 1:
-                x2, y2 = dfs(x, y, (x, y))
+                x2, y2 = dfs(x, y)
                 res.append([x, y, x2, y2])
         
+        res, R, C = [], len(land), len(land[0])
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+class Solution:
 [
