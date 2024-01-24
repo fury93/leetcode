88237cf86c0e133b01564530ae111da1e11328a6
fury93 <www.fanreala@gmail.com>
2024-01-24 05:35:34 +0300

    Update file 24/01/2024, 05:35:33

--- 286-WallsandGates/286-WallsandGates.py
+++ 286-WallsandGates/286-WallsandGates.py
@@ -1,16 +1,2 @@
-class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        q, R, C, E = deque(), len(rooms), len(rooms[0]), 2147483647
-        
-            if rooms[x][y] == 0:
-                q.append((x, y, 0))
-        
-            x, y, dist = q.popleft()
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < R and 0 <= ny < C and rooms[nx][ny] == E:
-                    rooms[nx][ny] = dist + 1
-        for x, y in product(range(R), range(C)):
-        while q:
-                    q.append((nx, ny, dist + 1))
+c
 [
