b716fffea00312accaa91d84afe63d29366b1b3e
fury93 <www.fanreala@gmail.com>
2024-01-24 18:42:01 +0300

    Update file 24/01/2024, 18:42:01

--- 305-NumberofIslandsII/305-NumberofIslandsII.py
+++ 305-NumberofIslandsII/305-NumberofIslandsII.py
@@ -1,28 +1,28 @@
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-
     def union(self, x, y):
-            return x
-    def find(self, x):
-        if x == self.root[x]:
-        self.cnt = 0
 
-        self.root = dict()
+        return self.root[x]
+        self.root[x] = self.find(self.root[x])
+            return x
+        if x == self.root[x]:
+    def find(self, x):
+
         self.rank = dict()
-class Solution:
-    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        res, ds = [], UnionFind()
-        for x, y in positions:
-            ds.add((x, y))
-            for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                xx, yy = x + dx, y + dy
-                if ds.isExist((xx, yy)):
-                    ds.union((xx, yy), (x, y))
+        self.cnt = 0
+class UnionFind:
+    def __init__(self):
+        self.root = dict()
             res.append(ds.getCnt())
                     
         return res
 
 
-class UnionFind:
-    def __init__(self):
+            for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                xx, yy = x + dx, y + dy
+                if ds.isExist((xx, yy)):
+                    ds.union((xx, yy), (x, y))
+class Solution:
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        res, ds = [], UnionFind()
+        for x, y in positions:
+            ds.add((x, y))
 3
