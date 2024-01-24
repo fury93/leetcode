e20f757760848dad7b2444db7a002b9a25c0154a
fury93 <www.fanreala@gmail.com>
2024-01-24 05:27:30 +0300

    Update file 24/01/2024, 05:27:30

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,23 +1,18 @@
-from itertools import product
 class Solution:
-    L = '1'
-    W = '0'
-
-    def numIslands(self, grid: List[List[str]]) -> int:
-        res, R, C = 0, len(grid), len(grid[0])
-
-        def dfs(x, y):
-            if 0 <= x < R and 0 <= y < C and grid[x][y] == self.L:
-                grid[x][y] = self.W
-                for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    dfs(x + dx, y + dy)
-
-        for i, j in product(range(R), range(C)):
-                if grid[i][j] == self.L:
-                    res += 1
-                    dfs(i, j)
-
-        return res
-
-
+    def wallsAndGates(self, rooms: List[List[int]]) -> 
+None:
+        """
+        Do not return anything, modify rooms in-place 
+instead.
+        """
+        q, R, C, E = deque(), len(rooms), len(rooms[0]), 
+2147483647
+        for x, y in product(range(R), range(C)):
+            if rooms[x][y] == 0:
+                q.append((x, y, 0))
+        print(q)
+        while q:
+            x, y, dist = q.popleft()
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)
+]:
 [
