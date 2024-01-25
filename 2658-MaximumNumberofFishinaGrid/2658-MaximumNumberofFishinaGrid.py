ff0c5b697d174aa52644add1941b4714c53958a7
fury93 <www.fanreala@gmail.com>
2024-01-25 04:51:46 +0300

    Update file 25/01/2024, 04:51:46

--- 2658-MaximumNumberofFishinaGrid/2658-MaximumNumberofFishinaGrid.py
+++ 2658-MaximumNumberofFishinaGrid/2658-MaximumNumberofFishinaGrid.py
@@ -1,2 +1,18 @@
- 
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> 
+int:
+        R, C = len(grid), len(grid[0])
+
+        def getFish(x, y):
+            fish, grid[x][y] = grid[x][y], 0
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), 
+(0, -1)]:
+                xx, yy = x + dx, y + dy
+                if 0 <= xx < R and 0 <= yy < C and grid
+[xx][yy] > 0:
+                    fish += getFish(xx, yy)
+
+            return fish
+        
+        res = 0
 [
