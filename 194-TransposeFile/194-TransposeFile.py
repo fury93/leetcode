83afa681ea8b667267f97dc40613978f4dcfad44
fury93 <www.fanreala@gmail.com>
2024-01-26 05:47:10 +0300

    Create file

---
+++ 194-TransposeFile/194-TransposeFile.py
@@ -0,0 +1,5 @@
+# Read from the file file.txt and print 
+its transposed content to stdout.
+head -1 file.txt | wc -w | xargs seq 1 | 
+xargs -I{} -n 1 sh -c "cut -d ' ' -f{} 
+file.txt | paste -sd ' ' -"
