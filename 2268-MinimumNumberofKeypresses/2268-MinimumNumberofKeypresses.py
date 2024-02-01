777f44c095b3ddd6c10cd8508cf1c021b22dc47a
fury93 <www.fanreala@gmail.com>
2024-02-02 01:35:17 +0300

    Update file 02/02/2024, 01:35:17

--- 2268-MinimumNumberofKeypresses/2268-MinimumNumberofKeypresses.py
+++ 2268-MinimumNumberofKeypresses/2268-MinimumNumberofKeypresses.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumKeypresses(self, s: str) -> int:
-        val = sorted(Counter(s).values(), reverse=True)
-        return sum(val[0:9]) + 2 * sum(val[9:18]) + 3 * sum(val[18:27])
+        freq = sorted(Counter(s).values(), reverse=True)
+        return sum((i // 9 + 1) * f for i, f in enumerate(freq))
+
 "
