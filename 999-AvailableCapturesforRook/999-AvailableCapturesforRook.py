9381b56354ef99beaca6178f4374e8130d578b3e
fury93 <www.fanreala@gmail.com>
2024-02-05 06:27:56 +0300

    Create file

---
+++ 999-AvailableCapturesforRook/999-AvailableCapturesforRook.py
@@ -0,0 +1,14 @@
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] == 'R':
+                    x0, y0 = i, j
+        res = 0
+        for i, j in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+            x, y = x0 + i, y0 + j
+            while 0 <= x < 8 and 0 <= y < 8:
+                if board[x][y] == 'p': res += 1
+                if board[x][y] != '.': break
+                x, y = x + i, y + j
+    def numRookCaptures(self, board):
+class Solution:
+[
