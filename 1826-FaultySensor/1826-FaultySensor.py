edf5c649ce1fd81fd781df138748e4b054fa2ed6
fury93 <www.fanreala@gmail.com>
2024-02-05 04:41:32 +0300

    Update file 05/02/2024, 04:41:31

--- 1826-FaultySensor/1826-FaultySensor.py
+++ 1826-FaultySensor/1826-FaultySensor.py
@@ -1,14 +1,15 @@
-class Solution: 
-    def minNumberOfHours(self, initialEnergy: int, initialExperience: int, energy: List[int], experience: List[int]) -> int:
-        ans = 0 
-        for x, y in zip(energy, experience): 
-            if initialEnergy <= x: 
-                ans += x + 1 - initialEnergy
-                initialEnergy = x + 1
-            if initialExperience <= y: 
-                ans += y + 1 - initialExperience
-                initialExperience = y + 1
-            initialEnergy -= x
-            initialExperience += y 
-        return ans 
-5
+        n = len(sensor1)
+        for i in range(n):
+            if sensor1[i] != sensor2[i]: # find point at which the sensors differ
+                break
+        j = k = i # init two pointers
+        while j < n-1 and sensor1[j] == sensor2[j+1]: # find to what point sensor1 data is shifted
+            j += 1
+        while k < n-1 and sensor1[k+1] == sensor2[k]: # find to what point sensor2 data is shifted
+            k += 1
+        # a sensor is faulty if the ptr reaches end of arr
+        # if both reach end of arr, it's impossible to tell
+        return -1 if k == n-1 and j == n-1 else 1 if j == n-1 else 2
+    def badSensor(self, sensor1: List[int], sensor2: List[int]) -> int:
+class Solution:
+[
