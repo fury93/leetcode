350e574885017ecc52cbfc1764de6be34d8eb914
fury93 <www.fanreala@gmail.com>
2024-02-06 22:22:26 +0300

    Update file 06/02/2024, 22:22:25

--- 1176-DietPlanPerformance/1176-DietPlanPerformance.py
+++ 1176-DietPlanPerformance/1176-DietPlanPerformance.py
@@ -1,13 +1,12 @@
 class Solution:
     def dietPlanPerformance(self, calories: List[int], k: int, lower: int, upper: int) -> int:
-        res, sm, l = 0, 0, 0
+        res, sm, k = 0, 0, k - 1
 
         for r, cal in enumerate(calories):
             sm += cal
             res -= sm < lower
             res += sm > upper
-            if r - l + 1 < k: continue
-            sm -= calories[l]
-            l += 1
+            if r - k < 0: continue
+            sm -= calories[r - k]
         return res
 [
