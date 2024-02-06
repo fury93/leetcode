da3178e1d62642f64bca3a9ee731089ea1d702df
fury93 <www.fanreala@gmail.com>
2024-02-06 23:28:10 +0300

    Create file

---
+++ 457-CircularArrayLoop/457-CircularArrayLoop.py
@@ -0,0 +1,15 @@
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n, visited = len(nums), set()
+        for i in range(n):
+            if i not in visited:
+                local_s = set()
+                while True:
+                    if i in local_s: return True
+                    if i in visited: break          
+                    visited.add(i)
+                    local_s.add(i)
+                    prev, i = i, (i + nums[i]) % n
+                    if prev == i or (nums[i] > 0) != (nums[prev] > 0): break
+        return False
+class Solution:
+[
