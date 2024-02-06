43998446ea54e13657b0ca4437960f9a22650dd0
fury93 <www.fanreala@gmail.com>
2024-02-07 01:19:39 +0300

    Update file 07/02/2024, 01:19:39

--- 2094-Finding3-DigitEvenNumbers/2094-Finding3-DigitEvenNumbers.py
+++ 2094-Finding3-DigitEvenNumbers/2094-Finding3-DigitEvenNumbers.py
@@ -3,7 +3,6 @@ class Solution:
         res, cnt1 = [], Counter(digits)
         for n in range(100, 999, 2):
             cnt2 = Counter(int(d) for d in str(n))
-            print(cnt1, cnt2)
             if cnt1 >= cnt2:
                 res.append(n)
         return res
