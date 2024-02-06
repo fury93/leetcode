826042600edbbbbd7d76d77fc86f19d004f15199
fury93 <www.fanreala@gmail.com>
2024-02-06 04:27:56 +0300

    Update file 06/02/2024, 04:27:56

--- 737-SentenceSimilarityII/737-SentenceSimilarityII.py
+++ 737-SentenceSimilarityII/737-SentenceSimilarityII.py
@@ -1,18 +1,18 @@
 
-class UnionFind:
-            if w1 != w2 and not uf.connected(w1, w2):
-                return False
-
         return True
+
+class UnionFind:
+    def __init__(self):
+        uf = UnionFind()
+        for w1, w2 in similarPairs:
             uf.union(w1, w2)
         
         for w1, w2 in zip(sentence1, sentence2):
-
-        uf = UnionFind()
-        uf.find('blabla')
-        for w1, w2 in similarPairs:
+            if w1 != w2 and not uf.connected(w1, w2):
+                return False
+class Solution:
     def areSentencesSimilarTwo(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
         if len(sentence1) != len(sentence2):
             return False
-class Solution:
+
 [
