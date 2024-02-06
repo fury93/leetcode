88934cf6c248542552f1700f8d112c8662b4434c
fury93 <www.fanreala@gmail.com>
2024-02-07 00:43:20 +0300

    Create file

---
+++ 1694-ReformatPhoneNumber/1694-ReformatPhoneNumber.py
@@ -0,0 +1,14 @@
+            res.append(''.join(nums[i:i+3]))
+
+        if len(nums) - 4 == i:
+            last = res[-1]
+            res[-1] = last[:2]
+            res.append(last[2] + nums[-1])
+        
+        return '-'.join(res)
+        for i in range(0, len(nums)-1, 3):
+        res, i = [], 0
+        nums = [n for n in number if n.isdigit()]
+    def reformatNumber(self, number: str) -> str:
+class Solution:
+"
