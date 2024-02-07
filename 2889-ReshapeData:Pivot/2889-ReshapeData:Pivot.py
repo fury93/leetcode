37aaca93011369048480cff785c6982a84696fac
fury93 <www.fanreala@gmail.com>
2024-02-07 22:25:44 +0300

    Create file

---
+++ 2889-ReshapeData:Pivot/2889-ReshapeData:Pivot.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    ans = weather.pivot(index='month', columns='city', values='temperature')
+    return ans
+[object Object]
