de7c21f4c607b0542e5cc475ebe04a1366c32559
fury93 <www.fanreala@gmail.com>
2024-02-07 22:12:27 +0300

    Update file 07/02/2024, 22:12:27

--- 2504-ConcatenatetheNameandtheProfession/2504-ConcatenatetheNameandtheProfession.py
+++ 2504-ConcatenatetheNameandtheProfession/2504-ConcatenatetheNameandtheProfession.py
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-select 
-a.symbol as metal,
-b.symbol as nonmetal
-from 
-Elements as a,
-Elements as b
-where a.type= "Metal" and b.type="Nonmetal"
+SELECT person_id, CONCAT(name, '(', SUBSTRING(profession, 1, 1), ')') AS name
+FROM Person p
+ORDER BY person_id DESC
 [object Object]
