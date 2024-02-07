5e269ae8435b452c7392d3599334e5f69d027906
fury93 <www.fanreala@gmail.com>
2024-02-07 22:16:31 +0300

    Create file

---
+++ 2885-RenameColumns/2885-RenameColumns.py
@@ -0,0 +1,13 @@
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.rename(
+        columns={
+            "id": "student_id",
+            "first": "first_name",
+            "last": "last_name",
+            "age": "age_in_years",
+        }
+    )
+    return students
+
+[object Object]
