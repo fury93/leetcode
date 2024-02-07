62946d8171680cbbbd26e5ac598c55fab84a38ce
fury93 <www.fanreala@gmail.com>
2024-02-07 22:13:53 +0300

    Create file

---
+++ 2990-LoanTypes/2990-LoanTypes.py
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT DISTINCT L1.user_id
+FROM Loans L1
+JOIN Loans L2 ON L1.user_id = L2.user_id
+WHERE L1.loan_type = 'Mortgage' AND L2.loan_type = 'Refinance'
+ORDER BY L1.user_id;
+[object Object]
