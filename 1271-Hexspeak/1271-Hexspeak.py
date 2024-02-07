df23f522b8735c00afa2ed2656aa7a4d7d8b5541
fury93 <www.fanreala@gmail.com>
2024-02-07 18:27:04 +0300

    Create file

---
+++ 1271-Hexspeak/1271-Hexspeak.py
@@ -0,0 +1,5 @@
+class Solution:
+    def toHexspeak(self, num: str) -> str:
+        s = hex(int(num)).upper()[2 :].translate(str.maketrans('01', 'OI'))
+        return 'ERROR' if any(c.isdigit() for c in s) else s
+"
