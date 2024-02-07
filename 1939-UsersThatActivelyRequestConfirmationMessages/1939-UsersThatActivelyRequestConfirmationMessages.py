9047aa7e40957fee417035647ab58943239d91ef
fury93 <www.fanreala@gmail.com>
2024-02-07 22:10:30 +0300

    Create file

---
+++ 1939-UsersThatActivelyRequestConfirmationMessages/1939-UsersThatActivelyRequestConfirmationMessages.py
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select
+distinct c.user_id
+from confirmations c
+join confirmations d
+on c.user_id=d.user_id and c.time_stamp>d.time_stamp and 
+timestampdiff(second, d.time_stamp,c.time_stamp)<=86400
+[object Object]
