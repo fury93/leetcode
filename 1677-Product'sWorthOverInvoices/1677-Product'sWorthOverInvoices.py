31b1a0ce2c7e21915e7a66c6c1c1e7dd3529d3b7
fury93 <www.fanreala@gmail.com>
2024-02-07 22:09:34 +0300

    Create file

---
+++ 1677-Product'sWorthOverInvoices/1677-Product'sWorthOverInvoices.py
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT p.name, IFNULL(SUM(rest),0) rest, IFNULL(SUM(paid),0) paid, IFNULL(SUM(canceled),0) canceled, IFNULL(SUM(refunded),0) refunded 
+FROM product p
+LEFT JOIN invoice i USING(product_id)
+GROUP BY p.name
+ORDER BY p.name
+[object Object]
