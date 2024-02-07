fcfe780a5cc131e56369906a11ef4e12c730883b
fury93 <www.fanreala@gmail.com>
2024-02-07 04:03:50 +0300

    Create file

---
+++ 2374-NodeWithHighestEdgeScore/2374-NodeWithHighestEdgeScore.py
@@ -0,0 +1,11 @@
+
+        for fromNode, toNode in enumerate(edges):
+            score[toNode] += fromNode
+
+        maxIndex = 0
+        for idx, val in enumerate(score):
+            if val > score[maxIndex]:
+        score = [0] * len(edges)
+    def edgeScore(self, edges: List[int]) -> int:
+class Solution:
+[
