5c5ed5c88b9f235d7517b605ab1fe8713f024ce7
fury93 <www.fanreala@gmail.com>
2024-02-07 22:03:07 +0300

    Create file

---
+++ 1082-SalesAnalysisI/1082-SalesAnalysisI.py
@@ -0,0 +1,14 @@
+) 
+SELECT 
+  seller_id 
+FROM 
+    seller_id
+  GROUP BY 
+  FROM 
+    Sales 
+  SELECT 
+    seller_id, 
+    SUM(price) AS total_price 
+# Write your MySQL query statement below
+WITH aggregated_sales AS (
+[object Object]
