51389a86bed8986483f4f9ef072c103da67839f1
fury93 <www.fanreala@gmail.com>
2024-02-07 22:05:40 +0300

    Create file

---
+++ 1322-AdsPerformance/1322-AdsPerformance.py
@@ -0,0 +1,13 @@
+            (SUM(action = 'Clicked') / (SUM(action = 'Clicked') + SUM(action = 'Viewed'))) * 100,
+            0
+        ), 
+        2
+    ) AS ctr
+FROM
+    Ads
+GROUP BY
+    ad_id
+ORDER BY
+    ctr DESC,
+    ad_id ASC;
+[object Object]
