3e9fcc9416bb3d6028d8c48c6766a767001163f9
fury93 <www.fanreala@gmail.com>
2024-02-07 22:03:26 +0300

    Create file

---
+++ 1113-ReportedPosts/1113-ReportedPosts.py
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT
+    extra AS report_reason,
+    COUNT(DISTINCT post_id) AS report_count
+FROM Actions
+WHERE action_date = '2019-07-04' AND 
+      action = 'report'
+GROUP BY report_reason;
+[object Object]
