acd5c37100df8482879ca69b78a8af8027e957e6
fury93 <www.fanreala@gmail.com>
2024-02-07 18:24:41 +0300

    Create file

---
+++ 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).py
@@ -0,0 +1,21 @@
+            p = p.next[c]
+        p.flag = True
+
+
+class Solution:
+                p.next[c] = TrieNode()
+            if c not in p.next:
+        p = self.root
+        for c in word:
+    def insert(self, word):
+
+        self.root = TrieNode()
+    def __init__(self):
+class Trie:
+
+        self.next = dict()
+
+    def __init__(self):
+        self.flag = False
+class TrieNode:
+"
