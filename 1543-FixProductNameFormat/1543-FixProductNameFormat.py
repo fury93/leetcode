786106b766909435341ca7787a34c7067fb3279f
fury93 <www.fanreala@gmail.com>
2024-02-07 22:08:24 +0300

    Create file

---
+++ 1543-FixProductNameFormat/1543-FixProductNameFormat.py
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT LOWER(TRIM(product_name)) product_name, DATE_FORMAT(sale_date, "%Y-%m") sale_date, count(sale_id) total
+FROM sales
+GROUP BY 1, 2
+ORDER BY 1, 2
+[object Object]
