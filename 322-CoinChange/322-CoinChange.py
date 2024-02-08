9a010994ecf498a5acb76f368016ac09166f1fee
fury93 <www.fanreala@gmail.com>
2024-02-08 03:38:09 +0300

    Create file

---
+++ 322-CoinChange/322-CoinChange.py
@@ -0,0 +1,15 @@
+        toCheck = {n}
+        while toCheck:
+            cnt += 1
+            temp = set()
+            for x in toCheck:
+                for y in lst:
+                    if x == y:
+                        return cnt
+                    if x < y:
+                        break
+                    temp.add(x-y)
+            toCheck = temp
+
+        return cnt
+12
