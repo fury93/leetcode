b27d2175e6d707e227dd81a34e81a1dcf7900a1c
fury93 <www.fanreala@gmail.com>
2024-02-08 03:05:00 +0300

    Update file 08/02/2024, 03:04:59

--- 1999-SmallestGreaterMultipleMadeofTwoDigits/1999-SmallestGreaterMultipleMadeofTwoDigits.py
+++ 1999-SmallestGreaterMultipleMadeofTwoDigits/1999-SmallestGreaterMultipleMadeofTwoDigits.py
@@ -1,16 +1,6 @@
-        q = deque([])
-        mn, mx = min(digit1, digit2), max(digit1, digit2)
-    def findInteger(self, k: int, digit1: int, digit2: int) -> int:
-        if mn != 0: q.append(mn)
         if mx != mn: q.append(mx)
 
         while q:
             num = q.popleft()
-            if num > 2 ** 31 - 1:
-                continue
-            if num > k and num % k == 0:
-                return num
-            q.append(num * 10 + mn)
-            if mn != mx: q.append(num * 10 + mx)
-        return -1
+        if mn != 0: q.append(mn)
 2
