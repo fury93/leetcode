07680414fa6954ba60b2834bf5da876c0c84bd14
fury93 <www.fanreala@gmail.com>
2024-02-09 12:00:49 +0300

    Create file

---
+++ 368-LargestDivisibleSubset/368-LargestDivisibleSubset.py
@@ -0,0 +1,22 @@
+                    subset = EDS(p)
+                    if len(maxSubset) < len(subset):
+                        maxSubset = subset
+            
+            # extend the found max subset with the current tail.
+            maxSubset = maxSubset.copy()
+            maxSubset.append(tail)
+            
+                if tail % nums[p] == 0:
+            for p in range(0, i):
+            # The value of EDS(i) depends on it previous elements
+            tail = nums[i]
+            maxSubset = []
+            
+            if i in memo:
+                return memo[i]
+        def EDS(i):
+            """ recursion with memoization """
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        
+class Solution:
+[
