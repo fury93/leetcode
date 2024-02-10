ee0f85d46dfff0cc53b99865a30de49f375b5a3a
fury93 <www.fanreala@gmail.com>
2024-02-11 01:40:46 +0300

    Update file 11/02/2024, 01:40:45

--- 276-PaintFence/276-PaintFence.py
+++ 276-PaintFence/276-PaintFence.py
@@ -1,8 +1,14 @@
-class Solution:
-    def numWays(self, n: int, k: int) -> int:
-        if n == 1:
-            return k
-        
-        two_posts_back = k
         one_post_back = k * k
+        
+        for i in range(3, n + 1):
+            curr = (k - 1) * (one_post_back + two_posts_back)
+            two_posts_back = one_post_back
+        two_posts_back = k
+        
+            return k
+            one_post_back = curr
+
+        if n == 1:
+    def numWays(self, n: int, k: int) -> int:
+class Solution:
 3
