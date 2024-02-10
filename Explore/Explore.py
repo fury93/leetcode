6fb646ff1e12aebf48b0aec06c8ffbc50d6574ae
fury93 <www.fanreala@gmail.com>
2024-02-10 03:13:25 +0300

    Update file 10/02/2024, 03:13:25

--- Explore/Explore.py
+++ Explore/Explore.py
@@ -1,27 +1,13 @@
-[
-class Solution:
-    # O(N^2)
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional
-[TreeNode]:
-        pos = {val: key for key, val in enumerate(inorder)}
-
-        def dfs(l, r):
-            if l > r: return None
-            root = TreeNode(postorder.pop())
-            root.right = dfs(pos[root.val]+1, r)
-            root.left = dfs(l, pos[root.val]-1)
-            return root
-
-        return dfs(0, len(inorder) - 1)
-    
-    def buildTree2(self, inorder: List[int], postorder: List[int]) -> Optional
-[TreeNode]:
-        if not inorder or not postorder:
-            return None
-        
-        root = TreeNode(postorder.pop())
-        inorderIndex = inorder.index(root.val)
-
-        root.right = self.buildTree(inorder[inorderIndex+1:], postorder)
-        root.left = self.buildTree(inorder[:inorderIndex], postorder)
-
+class·Solution:
+····def·countSubstrings(self,·S):
+········N·=·len(S)
+········ans·=·0
+········for·center·in·range(2*N·-·1):
+············left·=·center·//·2
+············right·=·left·+·center·%·2
+············while·left·>=·0·and·right·<·N·and·S[left]·==·S[right]:
+················ans·+=·1
+················left·-=·1
+················right·+=·1
+········return·ans
+"
