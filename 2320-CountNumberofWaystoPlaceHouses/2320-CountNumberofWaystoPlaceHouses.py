9626fa8c4600e21ffbdb6282b3b172f66179b2b5
fury93 <www.fanreala@gmail.com>
2024-02-11 20:05:01 +0300

    Create file

---
+++ 2320-CountNumberofWaystoPlaceHouses/2320-CountNumberofWaystoPlaceHouses.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countHousePlacements(self, n: int) -> int:
+        a, b, mod = 1, 1, 10**9 + 7
+        for i in range(n):
+            a, b = b, (a + b) % mod
+        return b * b % mod
+1
