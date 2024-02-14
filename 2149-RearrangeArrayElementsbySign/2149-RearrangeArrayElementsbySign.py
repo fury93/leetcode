ca3de4f37639754c7ee66ea214215daf73ea1f7a
fury93 <www.fanreala@gmail.com>
2024-02-14 06:03:14 +0300

    Create file

---
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.py
@@ -0,0 +1,15 @@
+
+    def rearrangeArray2(self, nums: List[int]) -> List[int]:
+        return res
+                i += 2
+                res[i] = n
+            else:
+                res[j] = n
+                j += 2
+            if n < 0:
+        for n in nums:
+        i, j = 0, 1
+        res = [0] * len(nums)
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+[
