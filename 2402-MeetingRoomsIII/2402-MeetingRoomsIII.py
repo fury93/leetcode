78606ba379e681f5bf9ed99556260637cb2f12cd
fury93 <www.fanreala@gmail.com>
2024-02-18 11:17:40 +0300

    Update file 18/02/2024, 11:17:39

--- 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -1,22 +1,22 @@
-class Solution:
-    def mostBooked2(self, n: int, meetings: List[List[int]]) -> int:
-        res = [0] * n
-        rooms = [(0,i) for i in range(n)] # (availabilityTime, roomId)
-        meetings.sort()
-        for start, end in meetings:
-            while rooms and rooms[0][0] < start:
-                _, room = heappop(rooms)
-                heappush(rooms, (start, room))
-            release, room = heappop(rooms)
-            heappush(rooms, (release + (end-start), room))
-            res[room] += 1
-        
-        return res.index(max(res))
-    
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        booked, free, freq = [], list(range(n)), defaultdict(int)
+        booked, free, res = [], list(range(n)), [0] * n
         meetings.sort()
 
         for start, end in meetings:
             while booked and booked[0][0] <= start:
+                _, room = heappop(booked)
+                heappush(free, room)
+            
+            if free:
+                room = heapq.heappop(free)
+                heappush(booked, (end, room))
+            else:
+                release, room = heappop(booked)
+                heappush(booked, (release + (end - start), room))
+            
+            res[room] += 1
+
+        return max(freq, key=freq.get)
+
+            freq[room] += 1
 2
