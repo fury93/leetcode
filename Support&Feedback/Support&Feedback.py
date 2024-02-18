10b9589fe345a1e2d281803d3bc04b00994e0b90
fury93 <www.fanreala@gmail.com>
2024-02-18 18:59:37 +0300

    Update file 18/02/2024, 18:59:37

--- Support&Feedback/Support&Feedback.py
+++ Support&Feedback/Support&Feedback.py
@@ -1,12 +1,15 @@
-class Solution:
-    def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        res, L = 0, len(words)
-        for i in range(L):
-            for j in range(i+1, L):
-                if lni > lnj: continue
 
-                if words[i] == words[j][:lni] and words[i] == words[j][-lni:lnj]:
-                lni, lnj = len(words[i]), len(words[j])
-                    res += 1
-        return res
+
+            elif (ln:= r - l + 1) > res.len:
+                res = Result(l, ln)
+        return words[res.start:res.start + res.len]
+
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        ans, cur_group = [], -1
+        for word, group in zip(words, groups):
+            if group != cur_group:
+                ans.append(word)
+                cur_group = group
+        return ans
 [
