632f7d76c27cc54d5b6f320df66f9503191a7764
fury93 <www.fanreala@gmail.com>
2024-02-18 11:03:06 +0300

    Update file 18/02/2024, 11:03:06

--- 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -1,14 +1,17 @@
         meetings.sort()
-        rooms = [(0,i) for i in range(n)] # (availabilityTime, roomId)
+        rooms = [[0,i] for i in range(n)] # (availabilityTime, roomId)
         res = [0] * n
         for start, end in meetings:
             while rooms and rooms[0][0] < start:
                 _, room = heappop(rooms)
-                heappush(rooms, (start, room))
+                heappush(rooms, [start, room])
             release, room = heappop(rooms)
-            heappush(rooms, (release + (end-start), room))
+            heappush(rooms, [release + (end-start), room])
             res[room] += 1
         
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
 class Solution:
+        return res.index(max(res))
+    
+    def mostBooked2(self, n: int, meetings: List[List[int]]) -> int:
 2
