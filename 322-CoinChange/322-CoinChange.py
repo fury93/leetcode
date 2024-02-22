67ea834466d7b7cf86c72ddc27aa78280c8893f7
fury93 <www.fanreala@gmail.com>
2024-02-23 02:42:22 +0300

    Update file 23/02/2024, 02:42:22

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,15 +1,20 @@
-        toCheck = {n}
-        while toCheck:
-            cnt += 1
-            temp = set()
-            for x in toCheck:
-                for y in lst:
-                    if x == y:
-                        return cnt
-                    if x < y:
-                        break
-                    temp.add(x-y)
-            toCheck = temp
+        @cache
+    def coinChange2(self, coins: List[int], amount: int) -> int:
+        @lru_cache(None)
+        def dp(i, amount):
+    def coinChange(self, coins: List[int], amount: int) -> int:
 
-        return cnt
-12
+        def dfs(i, need):
+            if need == 0: return 0
+        res = dfs(len(coins)-1, amount)
+
+            if need < 0: return math.inf
+
+            for j in range(i, -1, -1):
+    # Brute-force DFS
+                res = min(res, dfs(j, need - coins[j]) + 1)
+            res = math.inf
+class Solution:
+            return res
+        return res if res != math.inf else -1
+[
