81e42ef7dd2c7e6a24794d7f90b20aa5a4c1d918
fury93 <www.fanreala@gmail.com>
2024-02-23 17:55:53 +0300

    Update file 23/02/2024, 17:55:52

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.py
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.py
@@ -1,19 +1,11 @@
-class·Solution:
-····def·findCheapestPrice(self,·n:·int,·flights:·List[List[int]],·src:·int,·dst:·int,·k:·int)·->·int:
-········G·=·defaultdict(list)
-········for·v,·w,·d·in·flights:
-············G[v].append((w,d))
-········h·=·[(0,·0,·src)]
-········visited·=·set()
-········while·h:
-············d,·curk,·v·=·heappop(h)
-············if·v·==·dst:·return·d
-············if·(v,·curk)·in·visited·or·curk·>·k:·continue
-············visited.add((v,·curk))
-············for·w,·ecost·in·G[v]:
-················heappush(h,·(d+ecost,·curk·+·1,·w))
-········return·-1
-4
-[[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]]
-0
-3
+            else:
+                dp[i] = min(dp[i-1] + costs[0], dp[max(0, i-7)] + costs[1], dp[max(0, i-30)] + costs[2])
+            if i in dayset:
+        for i in range(1, len(dp)):
+        
+        dayset = set(days)
+        dp = [0] * (days[-1] + 1)
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+    # Bottom-Up (not optimized)
+class Solution:
+[
