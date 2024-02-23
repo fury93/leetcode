6456ba2beb456f7335e4441a4767275dcb7930f6
fury93 <www.fanreala@gmail.com>
2024-02-23 03:53:48 +0300

    Update file 23/02/2024, 03:53:48

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,19 +1,2 @@
-        @cache
-    def coinChange3(self, coins: List[int], amount: int) -> int:
-    # Brute-force DFS
-                    res = min(res, dfs(remain) + 1)
-            return res
-
-        return res if (res:=dfs(amount)) != math.inf else -1
-
-            for i in range(len(coins)):
-            res = math.inf
-
-            if need == 0: return 0
-        def dfs(need):
-        @cache
-    def coinChange(self, coins: List[int], amount: int) -> int:
 class Solution:
-                remain = need - coins[i]
-                if remain >= 0:
 [
