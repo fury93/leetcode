7cbcf6c2f07078502988b5b856f2356908af196a
fury93 <www.fanreala@gmail.com>
2024-02-23 05:58:46 +0300

    Update file 23/02/2024, 05:58:45

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,2 +1,12 @@
- 
-[
+        if n <= 2: return n
+
+        prev1 = 1
+        prev2 = 2
+
+        for i in range(2, n):
+            prev1, prev2 = prev2, prev1 + prev2
+        
+        return prev2
+    def climbStairs2(self, n: int) -> int:
+        d = {}
+2
