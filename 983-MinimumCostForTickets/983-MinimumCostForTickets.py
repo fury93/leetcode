873ad72c0111bd65524586b89265d153160d7a1f
fury93 <www.fanreala@gmail.com>
2024-02-23 17:48:06 +0300

    Create file

---
+++ 983-MinimumCostForTickets/983-MinimumCostForTickets.py
@@ -0,0 +1,11 @@
+            else:
+                return min(dp(day - d) + c for d, c in zip([1, 7, 30], costs))
+            if day in dayset:
+            if day < days[0]: return 0
+        def dp(day):
+        @cache
+        dayset = set(days)
+    def mincostTickets(self, days, costs):
+    # Top-Down
+class Solution:
+[
