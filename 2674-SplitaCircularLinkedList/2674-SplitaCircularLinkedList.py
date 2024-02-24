b2cbe96835b9fe446bf5cd1e1ac7258b799f7d95
fury93 <www.fanreala@gmail.com>
2024-02-24 14:50:59 +0300

    Update file 24/02/2024, 14:50:59

--- 2674-SplitaCircularLinkedList/2674-SplitaCircularLinkedList.py
+++ 2674-SplitaCircularLinkedList/2674-SplitaCircularLinkedList.py
@@ -1,19 +1,22 @@
-        end = cur
+        end = cur # save the last node
           
         mid = math.ceil(cnt / 2)
         while mid > 0:
             cur, mid = cur.next, mid - 1
 
-        start2 = cur.next
+        # 1(start1) -> 2(cur) -> 3(cur.next) -> 4(end)
             cur, cnt = cur.next, cnt + 1
         while cur.next != start1:
-        cnt, cur, end = 1, start1, None
+        cur, cnt = start1, 1
         end.next = start2
+    def splitCircularLinkedList(self, start1: Optional[ListNode]) -> List[Optional[ListNode]]:
+        # find the lenght and the last node
+        # after interation cur will be the last node for block1
+        start2 = cur.next
+class Solution:
         cur.next = start1
 
         return [start1, start2]
         
-
-
 
 [
