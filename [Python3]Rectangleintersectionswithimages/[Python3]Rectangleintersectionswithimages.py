a9ff13fd382cc206bd54535deb4d201761165a47
fury93 <www.fanreala@gmail.com>
2024-02-25 10:11:03 +0300

    Update file 25/02/2024, 10:11:03

--- [Python3]Rectangleintersectionswithimages/[Python3]Rectangleintersectionswithimages.py
+++ [Python3]Rectangleintersectionswithimages/[Python3]Rectangleintersectionswithimages.py
@@ -1,14 +1,25 @@
-            ax1, ay1 = a[0]
-            ax2, ay2 = a[1]
-            bx1, by1 = b[0]
-            bx2, by2 = b[1]
-
             overlapX = min(ax2, bx2) - max(ax1, bx1)
             overlapY = min(ay2, by2) - max(ay1, by1)
 
             if overlapX > 0 and overlapY > 0:
                 minSquareSide = min(overlapX, overlapY)
                 res = max(res, minSquareSide)
+        for ((ax1, ay1), (ax2, ay2)), ((bx1,·by1),·(bx2,·by2)) in combinations(coordinats, 2):
+        
+        coordinats = list(zip(bottomLeft, topRight))
+        res = 0 # the rectange side
 
         return res * res
+
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        return res * res
+
+                    res = max(res, minSquareSide)
+                    minSquareSide = min(overlapX, overlapY)
+                overlapX = min(ax2, bx2) - max(ax1, bx1)
+                overlapY = min(ay2, by2) - max(ay1, by1)
+
+                # move to the next i-th rectangle, no more intersactions between i-th and j-th rectangles
+                if overlapX < 0 and overlapY < 0: break 
+                if overlapX > 0 and overlapY > 0:
 [
