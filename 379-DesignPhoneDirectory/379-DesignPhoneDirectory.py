87d749820094023d70e755a80b35dc9d40cba479
fury93 <www.fanreala@gmail.com>
2024-02-27 02:34:20 +0300

    Update file 27/02/2024, 02:34:20

--- 379-DesignPhoneDirectory/379-DesignPhoneDirectory.py
+++ 379-DesignPhoneDirectory/379-DesignPhoneDirectory.py
@@ -1,14 +1,14 @@
             prev.next, prev = (cur:= Phone(i)), cur
-    def __init__(self, maxNumbers: int):
-        self.slots = [True] * maxNumbers # True -> is available
-        self.phones = Phone(-1) # backbone
-        prev = self.phones
         for i in range(maxNumbers):
-class Phone:
-    def __init__(self, val, next = None):
-        self.val = val
-        self.next = next
+        prev = self.phones
+        self.phones = Phone(-1) # list of available phones
+        self.slots = [True] * maxNumbers # status of all phones slots, True -> is available
+    def __init__(self, maxNumbers: int):
 
 class PhoneDirectory:
 
+        self.next = next
+        self.val = val
+    def __init__(self, val, next = None):
+class Phone:
 [
